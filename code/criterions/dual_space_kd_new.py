7134aaffc2f1f0a2a5b959aa83d4762aae125d8d
HoangTran223 <tranvuonghoang204@gmail.com>
2025-04-05 15:21:33 +0700

    a

--- code/criterions/dual_space_kd_new.py
+++ code/criterions/dual_space_kd_new.py
@@ -1,12 +1,13 @@
-﻿import math
+import math
 import torch
 from .various_divergence import VariousDivergence
-import torch.nn as nn
-
+from .ETP import ETP
 
 class DualSpaceKDWithCMA(VariousDivergence):
     def __init__(self, args, padding_id=-100) -> None:
         super().__init__(args, padding_id=padding_id)
+        self.args = args
+        self.etp = ETP()
 
     def forward(
         self, 
@@ -19,19 +20,14 @@ class DualSpaceKDWithCMA(VariousDivergence):
         model = distiller.student_model
         teacher_model = distiller.teacher_model
         self.distiller = distiller
+
         outputs = model(
             input_data["input_ids"],
             attention_mask=input_data["attention_mask"],
             position_ids=input_data.get("position_ids", None), 
             output_hidden_states=True
         )
-        logits = outputs.logits
-        log = {}
 
-        # Cross-entropy loss
-        loss = self.compute_cross_entropy_loss(
-            outputs.logits, output_data["label"], log=log
-        )[0]
 
         with torch.no_grad():
             teacher_model.eval()
@@ -39,12 +35,47 @@ class DualSpaceKDWithCMA(VariousDivergence):
                 input_data[f"teacher_{distiller.teacher_model_type}_input_ids"],
                 attention_mask=input_data[f"teacher_{distiller.teacher_model_type}_attention_mask"],
                 position_ids=input_data.get(f"teacher_{distiller.teacher_model_type}_position_ids", None), 
-                output_hidden_states=True)
+                output_hidden_states=True
+            )
+        
+        logits = outputs.logits
+        log = {}
+
+        loss = 0.0
+        # Cross-entropy loss
+        loss_ce = self.compute_cross_entropy_loss(
+            outputs.logits, output_data["label"], log=log
+        )[0]
+        log["loss_ce"] = loss_ce
+        
         
         kd_loss, log = self.compute_dual_space_kd_loss_with_cma(
             outputs, teacher_outputs, input_data, output_data, distiller, log
         )
-        loss = (1.0 - self.kd_rate) * loss + self.kd_rate * kd_loss
+        loss = loss_ce + self.kd_rate * kd_loss
+        log["kd_loss"] = kd_loss
+
+        hidden_state_student = outputs.hidden_states[-1]  # (batch_size, seq_len_student, hidden_dim_student)
+        hidden_state_student_first = outputs.hidden_states[0]
+        hidden_state_teacher = teacher_outputs.hidden_states[-1]  # (batch_size, seq_len_teacher, hidden_dim_teacher)
+        hidden_state_teacher_first = teacher_outputs.hidden_states[0]
+
+        ot_loss = 0.0
+        target = output_data["label"]
+        teacher_target = output_data[f"teacher_{distiller.teacher_model_type}_label"]
+        
+        pad_mask = target.ne(self.padding_id)
+        teacher_pad_mask = teacher_target.ne(self.padding_id)
+        
+        ot_loss_last, log = self.compute_etp_loss(distiller, hidden_state_student, hidden_state_teacher, pad_mask, teacher_pad_mask, log)
+        ot_loss_first, log = self.compute_etp_loss(distiller, hidden_state_student_first, hidden_state_teacher_first, pad_mask, teacher_pad_mask, log)
+        
+        ot_loss += (ot_loss_last + ot_loss_first)
+        log["ot_loss_last"] = ot_loss_last 
+        log["ot_loss_first"] = ot_loss_first
+        log["ot_loss"] = ot_loss
+
+        loss += ot_loss
         log["loss"] = loss
 
         accuracy = self.compute_token_accuracy(
@@ -56,6 +87,47 @@ class DualSpaceKDWithCMA(VariousDivergence):
             logging_output, batch_denom, log
         )
         return loss / batch_denom, logging_output
+    
+    
+    def compute_etp_loss(
+        self, distiller ,student_outputs, teacher_outputs, attention_mask_student, attention_mask_teacher, log, logits = False
+    ):
+        """
+        Compute OT loss between teacher and student outputs
+        
+        Args:
+            teacher_outputs: tensor of shape (batch_size, seq_len1, input_dim)
+            student_outputs: tensor of shape (batch_size, seq_len2, output_dim)
+            
+        Returns:
+            loss: scalar tensor
+        """
+    
+        if logits:
+            teacher_outputs = distiller.projectors["ot"](teacher_outputs)
+        else:
+            teacher_outputs = distiller.projectors["ot"](teacher_outputs)
+
+        batch_size = teacher_outputs.size(0)
+        total_loss = 0
+        for b in range(batch_size):
+            # Get sequences for current batch
+            teacher_seq = teacher_outputs[b]
+            student_seq = student_outputs[b]
+
+            teacher_mask = attention_mask_teacher[b]  # (seq_len1)
+            student_mask = attention_mask_student[b]  # (seq_len2)
+            
+            # Prune sequences based on the mask
+            teacher_seq = teacher_seq[teacher_mask.bool()]  # Shape: (valid_seq_len1, hidden_dim)
+            student_seq = student_seq[student_mask.bool()]  # Shape: (valid_seq_len2, hidden_dim)
+            
+            # Compute ETP loss
+            etp_loss, transp = self.etp(student_seq, teacher_seq)
+            total_loss += etp_loss
+        loss =  total_loss / (batch_size*4)
+        return loss, log
+        
     
     def compute_dual_space_kd_loss_with_cma(
         self, outputs, teacher_outputs, input_data, output_data, distiller, log
